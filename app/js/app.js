6b7e460bd779b27cda3e84b93dcfc58361f1fc14
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2021-12-07 20:17:44 +0200

    add project

---
+++ app/js/app.js
@@ -0,0 +1,18 @@
+// // Import jQuery module (npm i jquery)
+// import $ from 'jquery'
+// window.jQuery = $
+// window.$ = $
+
+// // Import vendor jQuery plugin example (not module)
+// require('~/app/libs/mmenu/dist/mmenu.js')
+import $ from 'jquery'
+
+import popper from 'popper.js';
+
+import bootstrap from 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+})
